cba93093be2e6912c1ad9e4630accc4ce4382d8e
Teodor Nistor <teo.g.nistor@gmail.com>
2017-07-13 18:44:52 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var tnmod = angular.module('TNmod', ['ngRoute']);
+// console.log ('Initiating Angular...')
+
+tnmod.config (['$routeProvider',
+  function ($routeProvider) {
+    $routeProvider.otherwise({templateUrl: 'foo.htm', controller: 'mainCtl'})
+  }
+]);
+
+tnmod.controller ('mainCtl', ['$scope',
+  function ($scope) {
+    $scope.fruit = [
+      {name: 'Banana', score: 7},
+      {name: 'Avocado', score: 10}
+    ];
+    $scope.bar = 'sh';
+  }
+]);
