d61116cbb101510905b81ca7eda829eb033ec323
Teo G Nistor <teo.g.nistor@gmail.com>
2020-04-13 12:24:48 +0100

    First commit - Vue site

---
+++ precache-manifest.ecee250ad0067452a486f1d5562de1b1.js
@@ -0,0 +1,218 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f7bd9f9794a7a93c189569c31acede",
+    "url": "/404.html"
+  },
+  {
+    "revision": "29c50a14026ccb3f01cc",
+    "url": "/css/chunk-103ff77a.1ff80783.css"
+  },
+  {
+    "revision": "2ada0b1c986768d99ee8",
+    "url": "/css/chunk-2f5cb18a.30592c39.css"
+  },
+  {
+    "revision": "f7244796155c1e998f97",
+    "url": "/css/chunk-e2f49838.025b3ffb.css"
+  },
+  {
+    "revision": "dd8dfbba61e44b69dc32",
+    "url": "/css/chunk-vendors.68551c11.css"
+  },
+  {
+    "revision": "22224285c1b91f6a2f31127cb0bc2e59",
+    "url": "/css/main.css"
+  },
+  {
+    "revision": "8069abc1e0968ff335480dbdf93c13d3",
+    "url": "/data/suhc/log.json"
+  },
+  {
+    "revision": "f71e63b9ae0604b69dc278663ef32b01",
+    "url": "/data/suhc/log_example.json"
+  },
+  {
+    "revision": "86703593662b61ac2b07ed919270e98e",
+    "url": "/data/suhc/quotes.json"
+  },
+  {
+    "revision": "313a65630d341645c13e4f2a0364381d",
+    "url": "/fonts/Roboto-Black.313a6563.woff"
+  },
+  {
+    "revision": "59eb3601394dd87f30f82433fb39dd94",
+    "url": "/fonts/Roboto-Black.59eb3601.woff2"
+  },
+  {
+    "revision": "cc2fadc3928f2f223418887111947b40",
+    "url": "/fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
+    "url": "/fonts/Roboto-BlackItalic.f75569f8.woff2"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "/fonts/Roboto-Bold.50d75e48.woff"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/fonts/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "4fe0f73cc919ba2b7a3c36e4540d725c",
+    "url": "/fonts/Roboto-BoldItalic.4fe0f73c.woff"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "/fonts/Roboto-BoldItalic.94008e69.woff2"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/fonts/Roboto-Light.c73eb1ce.woff"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/fonts/Roboto-Light.d26871e8.woff2"
+  },
+  {
+    "revision": "13efe6cbc10b97144a28310ebdeda594",
+    "url": "/fonts/Roboto-LightItalic.13efe6cb.woff"
+  },
+  {
+    "revision": "e8eaae902c3a4dacb9a5062667e10576",
+    "url": "/fonts/Roboto-LightItalic.e8eaae90.woff2"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/fonts/Roboto-Medium.1d659482.woff"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/fonts/Roboto-Medium.90d16760.woff2"
+  },
+  {
+    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
+    "url": "/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+  },
+  {
+    "revision": "83e114c316fcc3f23f524ec3e1c65984",
+    "url": "/fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/fonts/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "/fonts/Roboto-RegularItalic.4357beb8.woff2"
+  },
+  {
+    "revision": "f5902d5ef961717ed263902fc429e6ae",
+    "url": "/fonts/Roboto-RegularItalic.f5902d5e.woff"
+  },
+  {
+    "revision": "ad538a69b0e8615ed0419c4529344ffc",
+    "url": "/fonts/Roboto-Thin.ad538a69.woff2"
+  },
+  {
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "/fonts/Roboto-Thin.d3b47375.woff"
+  },
+  {
+    "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
+    "url": "/fonts/Roboto-ThinItalic.5b4a33e1.woff2"
+  },
+  {
+    "revision": "8a96edbbcd9a6991d79371aed0b0288e",
+    "url": "/fonts/Roboto-ThinItalic.8a96edbb.woff"
+  },
+  {
+    "revision": "541e65fb46e44ecdf7a9da8a9b881446",
+    "url": "/fonts/materialdesignicons-webfont.541e65fb.eot"
+  },
+  {
+    "revision": "c61b9c12f68ee1ba045a4b49dba29ca5",
+    "url": "/fonts/materialdesignicons-webfont.c61b9c12.woff2"
+  },
+  {
+    "revision": "fc03f7f15facede623faa7666c7d1f5a",
+    "url": "/fonts/materialdesignicons-webfont.fc03f7f1.ttf"
+  },
+  {
+    "revision": "ff13d121c1a1cf74d8e06bd39e1746c3",
+    "url": "/fonts/materialdesignicons-webfont.ff13d121.woff"
+  },
+  {
+    "revision": "e99dde1bf55d1eb0c6821ae2dd6bfa16",
+    "url": "/img/404.png"
+  },
+  {
+    "revision": "360842dce3f69f728aa77ae005424fe8",
+    "url": "/img/bkg.png"
+  },
+  {
+    "revision": "2d1afbb00c4b8fdd04c6e463a1f92d9f",
+    "url": "/img/landing.png"
+  },
+  {
+    "revision": "e477062eb0329805a7317683634e264d",
+    "url": "/img/suhc-logo-new.png"
+  },
+  {
+    "revision": "ca821eeb5e93204199e0d562061fa9cb",
+    "url": "/img/suhc-logo-old.png"
+  },
+  {
+    "revision": "66e14c70f312bb6e805fcb5a154237ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0123b67484ff66b76e0b",
+    "url": "/js/app.bf948fc4.js"
+  },
+  {
+    "revision": "29c50a14026ccb3f01cc",
+    "url": "/js/chunk-103ff77a.15ec0a9d.js"
+  },
+  {
+    "revision": "5c80f87ed5693891c223",
+    "url": "/js/chunk-2d20fec4.cfec9f38.js"
+  },
+  {
+    "revision": "d99e2eae56558cb2c2b8",
+    "url": "/js/chunk-2d2138f6.f577ea03.js"
+  },
+  {
+    "revision": "78dbb202c5dd17dcb52b",
+    "url": "/js/chunk-2d21b46c.7e3eae89.js"
+  },
+  {
+    "revision": "2ada0b1c986768d99ee8",
+    "url": "/js/chunk-2f5cb18a.ba8b68c2.js"
+  },
+  {
+    "revision": "880f9403be1d406a3379",
+    "url": "/js/chunk-741f5406.ca43146d.js"
+  },
+  {
+    "revision": "f7244796155c1e998f97",
+    "url": "/js/chunk-e2f49838.f7555131.js"
+  },
+  {
+    "revision": "dd8dfbba61e44b69dc32",
+    "url": "/js/chunk-vendors.4c00281c.js"
+  },
+  {
+    "revision": "6ef2963b754796cdb4130c0ffe4853eb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
